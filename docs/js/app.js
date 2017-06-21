6c3131723cefbd3555a32892cbbdced94312eee6
Hunter Long <info@socialeck.com>
2017-06-20 18:11:31 -0600

    new txt form

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,14 +1,16 @@
-$( document ).ready(function() {
+var MY_ADDRESS = '0x0E9E062D7e60C8a6A406488631DAE1c5f6dB0e7D'
 
-});
+var costWei = 0;
 
+const abi = [{"constant":true,"inputs":[],"name":"enabled","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"phoneNumber","type":"string"},{"name":"textBody","type":"string"}],"name":"sendText","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[],"name":"enableContract","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"withdraw","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pauseContract","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"costWei","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"price","type":"uint256"}],"name":"changeCost","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newCost","type":"uint256"}],"name":"UpdateCost","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newStatus","type":"string"}],"name":"UpdateEnabled","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"number","type":"string"},{"indexed":false,"name":"message","type":"string"}],"name":"NewText","type":"event"}];
 
+var deployedContract;
 
 
 function SubmitTextForm() {
 
-  var phone = $("#dddd");
-  var message = $("#dddd");
+  var phone = $("#number").val();
+  var message = $("#bodymsg").val();
 
   $.ajax({
     url: "https://cjx.io/encrypt",type: "POST",data: "value="+encodeURI(phone),
@@ -27,21 +29,12 @@ function SubmitTextForm() {
             }
           });
       }
-    });
+  });
 
 
 
 }
 
-
-
-var MY_ADDRESS = '0x0E9E062D7e60C8a6A406488631DAE1c5f6dB0e7D'
-
-var costWei = 0;
-
-const abi = [{"constant":true,"inputs":[],"name":"enabled","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"phoneNumber","type":"string"},{"name":"textBody","type":"string"}],"name":"sendText","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[],"name":"enableContract","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"withdraw","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pauseContract","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"costWei","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"price","type":"uint256"}],"name":"changeCost","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newCost","type":"uint256"}],"name":"UpdateCost","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newStatus","type":"string"}],"name":"UpdateEnabled","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"number","type":"string"},{"indexed":false,"name":"message","type":"string"}],"name":"NewText","type":"event"}];
-
-var deployedContract;
 
 
 
@@ -86,7 +79,7 @@ function SendText(num,body) {
 
   deployedContract.sendText(num, body, {value: costWei, gas: 200000}, function(error, result){
     if(!error)
-      console.log(result)
+      console.log(result);
     else
       console.error(error);
   });
